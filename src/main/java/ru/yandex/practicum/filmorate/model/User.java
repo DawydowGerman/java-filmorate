79a73bf20d4b62b8bf4c9942d8f347239d75e826
German Davydov <dawydow.german@yandex.ru>
2025-07-29 15:29:25 +0300

    feat(User): implement validated friend operations
    Add null checks using Objects.requireNonNull for 3 methods.
    Prevent self-friending through ValidationException.

--- src/main/java/ru/yandex/practicum/filmorate/model/User.java
+++ src/main/java/ru/yandex/practicum/filmorate/model/User.java
@@ -2,48 +2,51 @@ package ru.yandex.practicum.filmorate.model;
 
 import lombok.Builder;
 import lombok.Data;
+import lombok.NonNull;
+import ru.yandex.practicum.filmorate.exception.ValidationException;
 
 import java.time.LocalDate;
 import java.util.HashSet;
 import java.util.Set;
+import java.util.Objects;
 
 @Data
 @Builder
 public class User {
     @Builder.Default
-    private Long id = Long.valueOf(0);
+    private Long id = 0L;
+
     @Builder.Default
-    private String email = "Default email";
+    @NonNull
+    private String email = "";
+
     @Builder.Default
-    private String login = "Default login";
+    @NonNull
+    private String login = "";
+
     @Builder.Default
-    private String name = "Default name";
+    @NonNull
+    private String name = "";
+
     @Builder.Default
-    private LocalDate birthday = LocalDate.of(2000, 12, 01);
+    private LocalDate birthday = LocalDate.of(2000, 12, 1);
+
     @Builder.Default
     private Set<Long> friends = new HashSet<>();
 
     public void setToFriends(Long id) {
+        Objects.requireNonNull(id, "Friend ID cannot be null");
+        if (id.equals(this.id)) throw new ValidationException("Cannot add self as friend");
         friends.add(id);
     }
 
-    public void removeFromFriends(Long id) {
-        friends.remove(id);
+    public boolean removeFromFriends(Long id) {
+        Objects.requireNonNull(id, "Friend ID cannot be null");
+        return friends.remove(id);
     }
 
     public boolean isFriend(Long id) {
+        Objects.requireNonNull(id, "Friend ID cannot be null");
         return friends.contains(id);
-    }
-
-    @Override
-    public String toString() {
-        return "User{" +
-                "id=" + id +
-                ", email='" + email + '\'' +
-                ", login='" + login + '\'' +
-                ", name='" + name + '\'' +
-                ", birthday=" + birthday +
-                ", friends=" + friends +
-                '}';
     }
 }
